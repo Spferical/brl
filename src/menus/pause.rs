26f584e7e645567aa74caf622f7c4bb54ecb2afe
Matthew Pfeiffer <spferical@gmail.com>
2025-11-09 13:55:05 -0500

    Switch pause menu to use egui

--- src/menus/pause.rs
+++ src/menus/pause.rs
@@ -1,43 +1,40 @@
 //! The pause menu.
 
-use bevy::{input::common_conditions::input_just_pressed, prelude::*};
+use bevy::prelude::*;
+use bevy_egui::{EguiContexts, EguiPrimaryContextPass, egui};
 
-use crate::{menus::Menu, screens::Screen, theme::widget};
+use crate::{menus::Menu, screens::Screen};
 
 pub(super) fn plugin(app: &mut App) {
-    app.add_systems(OnEnter(Menu::Pause), spawn_pause_menu);
     app.add_systems(
-        Update,
-        go_back.run_if(in_state(Menu::Pause).and(input_just_pressed(KeyCode::Escape))),
+        EguiPrimaryContextPass,
+        pause_menu.run_if(in_state(Menu::Pause)),
     );
 }
 
-fn spawn_pause_menu(mut commands: Commands) {
-    commands.spawn((
-        widget::ui_root("Pause Menu"),
-        GlobalZIndex(2),
-        DespawnOnExit(Menu::Pause),
-        children![
-            widget::header("Game paused"),
-            widget::button("Continue", close_menu),
-            widget::button("Settings", open_settings_menu),
-            widget::button("Quit to title", quit_to_title),
-        ],
-    ));
-}
-
-fn open_settings_menu(_: On<Pointer<Click>>, mut next_menu: ResMut<NextState<Menu>>) {
-    next_menu.set(Menu::Settings);
-}
-
-fn close_menu(_: On<Pointer<Click>>, mut next_menu: ResMut<NextState<Menu>>) {
-    next_menu.set(Menu::None);
-}
-
-fn quit_to_title(_: On<Pointer<Click>>, mut next_screen: ResMut<NextState<Screen>>) {
-    next_screen.set(Screen::Title);
-}
-
-fn go_back(mut next_menu: ResMut<NextState<Menu>>) {
-    next_menu.set(Menu::None);
+fn pause_menu(
+    mut contexts: EguiContexts,
+    mut next_menu: ResMut<NextState<Menu>>,
+    mut next_screen: ResMut<NextState<Screen>>,
+    keyboard_input: Res<ButtonInput<KeyCode>>,
+) {
+    let Ok(ctx) = contexts.ctx_mut() else {
+        return;
+    };
+    ctx.style_mut(crate::theme::use_menu_theme);
+    egui::Window::new("Pause Menu")
+        .resizable(false)
+        .collapsible(false)
+        .anchor(egui::Align2::CENTER_CENTER, egui::Vec2::new(0.0, 0.0))
+        .show(ctx, |ui| {
+            if ui.button("Continue").clicked() || keyboard_input.just_pressed(KeyCode::Escape) {
+                next_menu.set(Menu::None);
+            }
+            if ui.button("Settings").clicked() {
+                next_menu.set(Menu::Settings);
+            }
+            if ui.button("Quit to title").clicked() {
+                next_screen.set(Screen::Title);
+            }
+        });
 }
