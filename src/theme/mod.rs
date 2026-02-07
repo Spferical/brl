9b62032f23e3ac9b0859aec3ba370365850a626c
Matthew Pfeiffer <spferical@gmail.com>
2026-02-07 17:58:16 -0500

    Drop once_cell dep

--- src/theme/mod.rs
+++ src/theme/mod.rs
@@ -3,13 +3,15 @@
 // Unused utilities may trigger this lints undesirably.
 #![allow(dead_code)]
 
+use std::sync::LazyLock;
+
 use bevy::prelude::*;
 use bevy_egui::egui;
-use once_cell::sync::Lazy;
 
 pub(super) fn plugin(_app: &mut App) {}
 
-pub static TITLE_STYLE: Lazy<egui::TextStyle> = Lazy::new(|| egui::TextStyle::Name("Title".into()));
+pub static TITLE_STYLE: LazyLock<egui::TextStyle> =
+    LazyLock::new(|| egui::TextStyle::Name("Title".into()));
 
 pub fn use_menu_theme(style: &mut egui::Style) {
     style.visuals.widgets.noninteractive.fg_stroke.color = bevy_egui::egui::Color32::WHITE;
