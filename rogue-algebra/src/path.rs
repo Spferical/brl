2bf8a0f164f697de4f00b9840ba4e2ae4c1eeba9
Matthew Pfeiffer <spferical@gmail.com>
2026-03-02 18:00:54 -0500

    rogue-algebra: allow custom node types

--- rogue-algebra/src/path.rs
+++ rogue-algebra/src/path.rs
@@ -1,24 +1,28 @@
-use std::collections::{HashMap, HashSet};
+use std::{
+    collections::{HashMap, HashSet},
+    hash::Hash,
+};
 
-use crate::Pos;
-
-pub fn dfs(starts: &[Pos], reachable: impl FnMut(Pos) -> Vec<Pos>) -> impl Iterator<Item = Pos> {
+pub fn dfs<Pos: Clone + Hash + Eq>(
+    starts: &[Pos],
+    reachable: impl FnMut(Pos) -> Vec<Pos>,
+) -> impl Iterator<Item = Pos> {
     Dfs {
         stack: starts.to_vec(),
-        visited: starts.iter().copied().collect::<HashSet<_>>(),
+        visited: starts.iter().cloned().collect::<HashSet<_>>(),
         reachable,
         to_emit: starts.to_vec(),
     }
 }
 
-struct Dfs<R: FnMut(Pos) -> Vec<Pos>> {
+struct Dfs<Pos: Clone + Hash + Eq, R: FnMut(Pos) -> Vec<Pos>> {
     stack: Vec<Pos>,
     visited: HashSet<Pos>,
     reachable: R,
     to_emit: Vec<Pos>,
 }
 
-impl<R: FnMut(Pos) -> Vec<Pos>> Iterator for Dfs<R> {
+impl<Pos: Clone + Hash + Eq, R: FnMut(Pos) -> Vec<Pos>> Iterator for Dfs<Pos, R> {
     type Item = Pos;
 
     fn next(&mut self) -> Option<Self::Item> {
@@ -29,8 +33,8 @@ impl<R: FnMut(Pos) -> Vec<Pos>> Iterator for Dfs<R> {
             if let Some(p) = self.stack.pop() {
                 let mut reachable = (self.reachable)(p);
                 reachable.retain(|p| !self.visited.contains(p));
-                self.visited.extend(reachable.iter());
-                self.stack.extend(reachable.iter());
+                self.visited.extend(reachable.iter().cloned());
+                self.stack.extend(reachable.iter().cloned());
                 self.to_emit.extend(reachable);
             } else {
                 return None;
@@ -39,22 +43,22 @@ impl<R: FnMut(Pos) -> Vec<Pos>> Iterator for Dfs<R> {
     }
 }
 
-pub fn bfs_paths<T: IntoIterator<Item = Pos>>(
+pub fn bfs_paths<Pos: Clone + Hash + Eq, T: IntoIterator<Item = Pos>>(
     starts: &[Pos],
     maxdist: usize,
     reachable: impl FnMut(Pos) -> T,
 ) -> impl Iterator<Item = Vec<Pos>> {
     Bfs {
-        periphery: starts.iter().map(|p| vec![*p]).collect(),
+        periphery: starts.iter().map(|p| vec![p.clone()]).collect(),
         new_periphery: vec![],
-        visited: starts.iter().copied().collect::<HashSet<_>>(),
+        visited: starts.iter().cloned().collect::<HashSet<_>>(),
         reachable,
-        to_emit: starts.iter().map(|p| vec![*p]).collect(),
+        to_emit: starts.iter().map(|p| vec![p.clone()]).collect(),
         maxdist,
     }
 }
 
-struct Bfs<T: IntoIterator<Item = Pos>, R: FnMut(Pos) -> T> {
+struct Bfs<Pos: Clone + Hash + Eq, T: IntoIterator<Item = Pos>, R: FnMut(Pos) -> T> {
     periphery: Vec<Vec<Pos>>,
     new_periphery: Vec<Vec<Pos>>,
     visited: HashSet<Pos>,
@@ -63,7 +67,9 @@ struct Bfs<T: IntoIterator<Item = Pos>, R: FnMut(Pos) -> T> {
     maxdist: usize,
 }
 
-impl<T: IntoIterator<Item = Pos>, R: FnMut(Pos) -> T> Iterator for Bfs<T, R> {
+impl<Pos: Clone + Hash + Eq, T: IntoIterator<Item = Pos>, R: FnMut(Pos) -> T> Iterator
+    for Bfs<Pos, T, R>
+{
     type Item = Vec<Pos>;
 
     fn next(&mut self) -> Option<Self::Item> {
@@ -72,10 +78,10 @@ impl<T: IntoIterator<Item = Pos>, R: FnMut(Pos) -> T> Iterator for Bfs<T, R> {
                 return Some(path);
             }
             if let Some(mut path) = self.periphery.pop() {
-                let reachable = (self.reachable)(*path.last().unwrap()).into_iter();
-                for pos in reachable {
+                let reachable = (self.reachable)(path.last().unwrap().clone()).into_iter();
+                for pos in reachable.into_iter() {
                     if !self.visited.contains(&pos) {
-                        self.visited.insert(pos);
+                        self.visited.insert(pos.clone());
                         path.push(pos);
                         self.to_emit.push(path.clone());
                         if path.len() < self.maxdist {
@@ -93,16 +99,17 @@ impl<T: IntoIterator<Item = Pos>, R: FnMut(Pos) -> T> Iterator for Bfs<T, R> {
     }
 }
 
-pub fn build_dijkstra_map<T: IntoIterator<Item = Pos>>(
+pub fn build_dijkstra_map<Pos: Clone + Hash + Eq, T: IntoIterator<Item = Pos>>(
     starts: &[Pos],
     maxdist: usize,
     reachable: impl FnMut(Pos) -> T,
 ) -> HashMap<Pos, usize> {
     starts
         .iter()
-        .map(|p| (*p, 0))
+        .map(|p| (p.clone(), 0))
         .chain(
-            bfs_paths(starts, maxdist, reachable).map(|path| (*path.last().unwrap(), path.len())),
+            bfs_paths(starts, maxdist, reachable)
+                .map(|path| (path.last().unwrap().clone(), path.len())),
         )
         .collect()
 }
